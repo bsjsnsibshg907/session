a800b52ed0ddb20c4872cb29aff427b120bc7a6d
bsjsnsibshg907 <102121098+bsjsnsibshg907@users.noreply.github.com>
2023-05-21 02:43:29 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# session
+
+
+## Usage
+
+### Deploy to Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Kokosabet/StringSessionBot)
+
+
+- `API_ID` - Get this from [my.telegram.org](https://my.telegram.org/auth)
+- `API_HASH` - Get this from [my.telegram.org](https://my.telegram.org/auth)
+- `BOT_TOKEN` - Get this from [@BotFather](https://t.me/BotFather)
+- `DATABASE_URL` - Will be automatically added by Heroku.
+- `MUST_JOIN` - Username/ID of your telegram channel/group.
+
+## Functions
+
+- Generate Pyrogram user string session
+- Generate Pyrogram bot string session
+- Generate Pyrogram (Major) Version 1 and 2 string sessions
+- Generate Telethon string session
+
+
+
+
+
+
+
+
+
---
+++ data.py
@@ -0,0 +1,66 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+class Data:
+    generate_single_button = [InlineKeyboardButton("◉ ¦ بدء استخراج جلسه •", callback_data="generate")]
+
+    home_buttons = [
+        generate_single_button,
+        [InlineKeyboardButton(text="💸 ¦ الرئيسية •", callback_data="home")]
+    ]
+
+    generate_button = [generate_single_button]
+
+    buttons = [
+        generate_single_button,
+        [
+            InlineKeyboardButton("🗯 ¦ مساعدة •", callback_data="help"),
+            InlineKeyboardButton("🔎 ¦ حول •", callback_data="about")
+        ],
+               [InlineKeyboardButton("☣️ ¦ 𝒆𝒎 𝒔𝒐𝒖𝒓𝒄𝒆 •", url="https://t.me/pyth_on1")]
+    ]
+
+    START = f"""
+- مرحبا بك عزيزى {message.from_user.mention} ❤
+
+- من هنا يمكنك استخراج جلسات البيروجرام والتليثون . 💸
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ 
+- اذا كنت لا تثق في البوت ف توقف عن القراءه واحظر البوت ⚠️
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ 
+◉ ¦ 𝒎𝒂𝒅𝒆 𝒃𝒚 : [⛥ .✘𝙀𝘽𝙉 𝙈𝘼𝙎𝙍 🇪🇬⛧’!×](https://t.me/UG_U4)
+"""
+
+    HELP = """
+✨ ¦ اوامر البوت الكتابيه ..
+
+/about - لمعرفه معلومات حول البوت 💸
+
+/help - نفس نص هذة الرساله 💸
+
+/start - بدء البوت 💸
+
+/generate - استخراج جلسه 💸
+
+/cancel - ايقاف الاستخراج 💸
+
+/restart - اعادة تشغيل البوت وتحديث الملفات 💸
+
+/reload - تحديث ملفات البوت 💸
+"""
+
+    ABOUT = """
+❇️ ¦ حول هذا البوت ¦ ❇️
+
+◉ ¦ بوت استخراج جلسات للحساب 
+
+✦ pyrogram.
+✦ Telethon .
+✦ pyrogram [ V2 ] .
+✦ Telethon ( Bot ) .
+✦ pyrogram ( Bot ) « V2 » .
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ 
+🗯 [ ➥ Є𝒎 𝒔𝒐𝒖𝒓𝒄𝒆 ☠ ](https://t.me/pyth_on1) .
+
+🗯 [[⛥ .✘𝙀𝘽𝙉 𝙈𝘼𝙎𝙍 🇪🇬⛧’!×](https://t.me/UG_U4) .
+ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ 
+    """
